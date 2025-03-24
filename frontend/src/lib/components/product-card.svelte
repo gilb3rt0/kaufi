cc5a678b9a571f46fe867ab4e0c2c04dd0b1c810
gilb3rt0 <hello@gilb3r.to>
2025-03-24 07:17:05 +0100

    add product card component & implement

---
+++ frontend/src/lib/components/product-card.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Product } from '@medusajs/client-types';
+
+	const { product }: { product: Product } = $props();
+
+	const { thumbnail, id, title } = product;
+</script>
+
+<ul class="flex flex-col border-4 border-zinc-700">
+	<li class="overflow-hidden">
+		<img src={thumbnail} alt="" class="object-contain w-full h-full" />
+	</li>
+	<li
+		class="flex bg-zinc-700 p-2 text-fuchsia-400 transition-colors focus-within:text-emerald-400 hover:text-emerald-400"
+	>
+		<a href="/shop/product/{id}" class="flex flex-col gap-2">
+			<h3 class="font-medium md:text-2xl">{title}</h3>
+		</a>
+	</li>
+</ul>
