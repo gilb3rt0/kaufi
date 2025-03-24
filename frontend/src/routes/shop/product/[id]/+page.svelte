8eb974ae8d12be375954f74e1d811cd1b2a7ea20
gilb3rt0 <hello@gilb3r.to>
2025-03-24 07:43:51 +0100

    add product detail page

---
+++ frontend/src/routes/shop/product/[id]/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { error } from '@sveltejs/kit';
+	import type { PageProps } from './$types';
+
+	const { data }: PageProps = $props();
+	const product = data?.product;
+	if (!product) {
+		error(404);
+	}
+	const { images, title, description } = product;
+</script>
+
+<h2 class="text-7xl font-bold">
+	{title}
+</h2>
+{#if images}
+	<div class="grid grid-cols-2 gap-2 md:grid-cols-3">
+		{#each images as { url, metadata }}
+			<div class="border-4 border-violet-800">
+				<img src={url} alt="" />
+			</div>
+		{/each}
+	</div>
+{/if}
+<p>{description}</p>
